96f61a5fd44eb43c7ad837d5373cd18dd8d1add0
leon <leontyr@gmail.com>
2018-04-24 09:25:29 +0100

    100

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-$(document).ready(function() {
-// constructs the suggestion engine
-var engine = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: 'all.json',
-  remote: {
-    url: 'all.json'
-  }
-});
+$(document).ready(function () {
+  // constructs the suggestion engine
+  var engine = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: 'all.json',
+    remote: {
+      url: 'all.json'
+    }
+  });
 
-$('#bloodhound .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'states',
-  source: engine
-});
+  $('#bloodhound .typeahead').typeahead(null, {
+    name: 'states',
+    source: engine
+  });
 });
